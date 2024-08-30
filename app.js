5b79ee713aafe7c003c74b44f4b8e0357f537ff1
Ignacio Salto <nachosalto05@gmail.com>
2024-08-30 19:00:19 -0300

    Webserver listo

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ hbs.registerPartials(__dirname + '/views/partials');
 //Servir contenido estatisco (hacer publico el html)
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Ignacio Salto',
-        titulo: 'Curso de Node',
-    });
-});
-
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Ignacio Salto',
-        titulo: 'Curso de Node',
-    });
-});
-
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Ignacio Salto',
-        titulo: 'Curso de Node',
-    });
-});
+//app.get('/', (req, res) => {
+//    res.render('home', {
+//        nombre: 'Ignacio Salto',
+//        titulo: 'Curso de Node',
+//    });
+//});
+//
+//app.get('/generic', (req, res) => {
+//    res.render('generic', {
+//        nombre: 'Ignacio Salto',
+//        titulo: 'Curso de Node',
+//    });
+//});
+//
+//app.get('/elements', (req, res) => {
+//    res.render('elements', {
+//        nombre: 'Ignacio Salto',
+//        titulo: 'Curso de Node',
+//    });
+//});
 //En caso de que alguien quiera acceder al http por otra ruta y le lanzamos un error:
 app.get('*', (req, res) => {
-    res.send('404 / page not found')
+    res.sendFile('404 / page not found')
 });
 
 app.listen(port, () => {
